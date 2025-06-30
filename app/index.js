5622c5d8aed51ba3a7af6d71aba91ef5193243b6
Sagar170899 <chavan.g.sagar@gmail.com>
2025-07-01 00:05:13 +0530

    Code update commit

--- app/index.js
+++ app/index.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.get('/health', (req, res) => res.send('OK'));
 
-app.listen(3001, '0.0.0.0', () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000');
 });
